b5b3bd8cb3275dc9998ffc7b4d9b9fdbfeeb7c4d
Emily Wraight <e.wraight90@gmail.com>
2021-08-23 16:33:23 +0100

    Temperature Element JS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+}
 
+let apiKey = "fc23ccd0a04eee182512a62ffe2b81be";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kent&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
