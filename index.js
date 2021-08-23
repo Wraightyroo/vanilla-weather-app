0fca2de4d5b9e9612780b66f6d8edfabf44ec9bf
Emily Wraight <e.wraight90@gmail.com>
2021-08-23 17:03:30 +0100

    JS to show current temp / wind / humidity /description

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round (response.data.wind.speed);
 }
 
 let apiKey = "fc23ccd0a04eee182512a62ffe2b81be";
