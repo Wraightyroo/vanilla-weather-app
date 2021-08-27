9e253163cf31c453175650b1cf219fd56e6861ef
Emily Wraight <e.wraight90@gmail.com>
2021-08-27 11:51:22 +0100

    Display forecast data using API

--- index.js
+++ index.js
@@ -21,24 +21,35 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date (timestamp *1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+
+    return days [day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML =`<div class="row">`;
-    let days = ["Thurs", "Fri", "sat"];
-    days.forEach(function (day) {
+    forecast.forEach(function(forecastDay, index) {
+    if (index <5) {
+
     forecastHTML=
     forecastHTML +
     `   <div class="col">
-          <div class="weather-forecast-date">${day}</div>
-          <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" class="float-left" id="icon" width= "64"/>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)} </div>
+          <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" class="float-left" id="icon" width= "64"/>
           <div class="weather-forecast-temps">
-            <span class="weather-forecast-temp-max">20˚</span>
-            <span class="weather-forecast-temp-min">10˚</span>
+            <span class="weather-forecast-temp-max"> ${Math.round (forecastDay.temp.max)}˚</span>
+            <span class="weather-forecast-temp-min">${Math.round (forecastDay.temp.min)}˚</span>
           </div>
         </div>
          `; 
+    }
 });   
     forecastHTML = forecastHTML+`</div>`;    
     forecastElement.innerHTML = forecastHTML;
